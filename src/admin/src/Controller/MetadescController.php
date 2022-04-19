a32cca10fc39b17a0eab7ef57e0f9d3667ce8e58
Sergey Osipov <47226965+devstratum@users.noreply.github.com>
2022-04-19 04:47:09 +0300

    Add files via upload

---
+++ src/admin/src/Controller/MetadescController.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @package         Metadesc Component
+ * @version         0.52
+ * @author          Sergey Osipov <info@devstratum.ru>
+ * @website         https://devstratum.ru
+ * @copyright       Copyright (c) 2022 Sergey Osipov. All Rights Reserved
+ * @license         GNU General Public License v2.0
+ * @report          https://github.com/devstratum/metadesc/issues
+ */
+
+namespace Devstratum\Component\Metadesc\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Response\JsonResponse;
+use Joomla\Input\Json;
+
+/**
+ * Component Controller Ajax
+ */
+class MetadescController extends BaseController
+{
+    /**
+     * Constructor
+     *
+     * @param   array  $config  An optional associative array of configuration settings
+     * @throws
+     */
+    public function __construct($config = [])
+    {
+        // Add json input
+        $app = Factory::getApplication();
+        if ($data = new Json())
+        {
+            foreach ($data->getArray() as $name => $value)
+            {
+                $app->input->def($name, $value);
+            }
+        }
+
+        // Parent constructor
+        $config['default_task'] = false;
+        $config['default_view'] = false;
+
+        $result = parent::__construct($config);
+
+        return $result;
+    }
+
+    /**
+     * Method action article
+     *
+     * @throws
+     */
+    public function article()
+    {
+        $app = Factory::getApplication();
+
+        $response = [];
+
+        $id = $app->input->getInt('id');
+        $description = $app->input->getString('description');
+
+        if ($id && $description) {
+            $message = ['type' => 'success', 'text' => 'Update: Data!'];
+            $response = ['id' => $id, 'description' => $description];
+        } else {
+            $message = ['type' => 'danger', 'text' => 'Error: Data!'];
+        }
+
+        $this->setResponse($response, $message);
+    }
+
+    /**
+     * Method action category
+     *
+     * @throws
+     */
+    public function category()
+    {
+        $app = Factory::getApplication();
+
+        $response = [];
+
+        $id = $app->input->getInt('id');
+        $description = $app->input->getString('description');
+
+        if ($id && $description) {
+            $message = ['type' => 'success', 'text' => 'Update: Data!'];
+            $response = ['id' => $id, 'description' => $description];
+        } else {
+            $message = ['type' => 'danger', 'text' => 'Error: Data!'];
+        }
+
+        $this->setResponse($response, $message);
+    }
+
+    /**
+     * Method action response
+     *
+     * @param array $response
+     * @param array $message
+     *
+     * @throws
+     */
+    public function setResponse($response, $message)
+    {
+        $app = Factory::getApplication();
+        $code = (!empty($this->code)) ? $this->code : 200;
+
+        header('Content-Type: application/json');
+        echo new JsonResponse($response, $message);
+        $app->close($code);
+    }
+}
